bc50f7064333129d4e9c1a204bd3e77e570a7ff0
CSDNguoqiangzhang <1580321809@qq.com>
2018-08-30 16:33:49 +0800

    first commit

---
+++ build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="651897ea-662d-46a5-83cf-a7f7ac736b8b")
+public class DataBindingInfo {}
